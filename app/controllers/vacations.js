d64d19b5b10c73bf720df3fc4263aeb0860cda76
Juan <jsamesub@gmail.com>
2014-08-18 20:36:02 -0500

    trabajando duro

--- app/controllers/vacations.js
+++ app/controllers/vacations.js
@@ -1,6 +1,15 @@
 
 'use strict';
 
-exports.vacations = function(req, res){
+exports.init = function(req, res){
   res.render('vacations/new');
+};
+
+exports.index = function(req, res){
+  res.render('vacations/index');
+};
+
+exports.create = function(req, res){
+  console.log(req.body);
+  res.redirect('/vacations');//This redirect send the browser back to /vacations... /vacations go to the index//
 };
